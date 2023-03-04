dbf4ce646cf7e5cecd2e357627a90914bd8bc9a1
Daniel Junior <112529265+Dan1elJr@users.noreply.github.com>
2023-03-04 19:05:43 -0400

    Update JavaApplicationStart.java

--- estrutura_condicional_exercício_JAVA/JavaApplicationStart.java
+++ estrutura_condicional_exercício_JAVA/JavaApplicationStart.java
@@ -15,14 +15,19 @@ public class JavaApplicationStart {
      * @param args the command line arguments
      */
    public static void main(String[] args) {
-    Scanner s1 =new Scanner(System.in);
-    System.out.println("DIGITE UM NÚMERO:");
-    int x = s1.nextInt();
-    if(x%2==0)
-      System.out.println("O NÚMERO É PAR");
-    else
-      System.out.println("O NÚMERO É ÍMPAR");
-  }
+        Scanner sc= new Scanner (System.in);
+        System.out.println("Digite a sua idade");
+        int a= sc.nextInt();
+        
+        if(a>=18)
+            System.out.println("entrada liberada");
+        else
+            System.out.println("entrada negada");
+        
+        
+        
+      
+    }
     
     
 }
