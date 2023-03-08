27d8346db61730de84e0d189a1c35ebaefdd941a
Daniel Junior <112529265+Dan1elJr@users.noreply.github.com>
2023-03-08 14:40:51 -0400

    Add files via upload

---
+++ POO_GetterAndSetter_exercício_JAVA/exemplo010.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+package javaapplicationstart;
+
+/**
+ *
+ * @author Casal
+ */
+
+public class exemplo010 {
+    public static void main(String[] args) {
+      // Instanciando e inicializando os objetos p1 e p2
+      Pessoa p1 = new Pessoa("João","1111-1111");
+      Pessoa p2 = new Pessoa("Maria","2222-2222");
+
+      // Chamada ao método exibir em p1 e p2
+
+      p1.exibir();
+      p2.exibir();
+      
+    }
+}
+
